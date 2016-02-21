ec1755ff6f82222ed5e329521bf20324ce451130
Laszlo Dobos <dobos@complex.elte.hu>
2016-02-21 23:20:15 +0100

    Converted to a module

--- wifid.lua
+++ wifid.lua
@@ -1,102 +1,102 @@
-wifid = {}
-wifid.SSID = nil
-wifid.password = nil
-wifid.apcounter = 0
-wifid.hostname = "nodeMCU"
-wifid.ip = "192.168.0.81"
-wifid.netmask = "255.255.255.0"
-wifid.gateway = "192.168.0.1"
-wifid.server = "192.168.0.2"
-wifid.gpio = 8
-wifid.tmr = 0
-wifid.interval = 200
-wifid.counter = 0
-wifid.gotip = false
+local Wifid = {}
+Wifid.__index = Wifid
 
-function wifid.start()
-	gpio.mode(wifid.gpio, gpio.OUTPUT)
-	gpio.write(wifid.gpio, gpio.LOW)
-		
-	tmr.alarm(wifid.tmr, wifid.interval, 1, wifid.event)
-	
-	wifid.connect()
+function Wifid.new(msg, ssid, pass)
+	local self = setmetatable({}, Wifid)
+	self.msg = msg
+	self.SSID = ssid
+	self.password = pass
+	self.apcounter = 0
+	self.hostname = "nodeMCU"
+	self.ip = "192.168.0.81"
+	self.netmask = "255.255.255.0"
+	self.gateway = "192.168.0.1"
+	self.server = "192.168.0.2"
+	self.gpio = 8
+	self.tmr = 0
+	self.interval = 200
+	self.counter = 0
+	self.gotip = false
+	return self
 end
 
-function wifid.connect()
-	for i = 0, #wifid.SSID do
-		if pcall(wifid.init) then
+function Wifid:start()
+	gpio.mode(self.gpio, gpio.OUTPUT)
+	gpio.write(self.gpio, gpio.LOW)
+		
+	tmr.alarm(self.tmr, self.interval, 1, self:getCallback())
+	
+	self:connect()
+end
+
+function Wifid:getCallback()
+	return function() self:event() end
+end
+
+function Wifid:connect()
+	for i = 0, #self.SSID do
+		if pcall(function() self:init() end) then
 			wifi.sta.connect()
 		else
-			wifid.apcounter = wifid.apcounter + 1
+			self.apcounter = self.apcounter + 1
 		end
 	end
 end
 
-function wifid.init()
-	local i = (wifid.apcounter % #wifid.SSID) + 1
+function Wifid:init()
+	local i = (self.apcounter % #self.SSID) + 1
 	wifi.setmode(wifi.STATION)
-	wifi.sta.sethostname(wifid.hostname)
-	wifi.sta.setip(wifid)
-	wifi.sta.config(wifid.SSID[i], wifid.password[i], 0)
+	wifi.sta.sethostname(self.hostname)
+	wifi.sta.setip(self)
+	wifi.sta.config(self.SSID[i], self.password[i], 0)
 end
 
-function wifid.event()
-	local i = (wifid.apcounter % #wifid.SSID) + 1
+function Wifid:event()
+	local i = (self.apcounter % #self.SSID) + 1
 	local sta = wifi.sta.status()
-	local ssid = (wifid.SSID[i])
+	local ssid = (self.SSID[i])
 	local ip = ""
 	local led = 0
-	local msg = nil
 		
 	if (sta == wifi.STA_IDLE) then
 		led = 0
 	elseif (sta == wifi.STA_CONNECTING) then
-		led = math.floor(wifid.counter / 3) % 2
-		wifid.gotip = false
-		msg = { "wifi connecting to", ssid }
+		led = math.floor(self.counter / 3) % 2
+		self.gotip = false
+		self.msg.messages = { "wifi connecting to", ssid }
 	elseif (sta == wifi.STA_WRONGPWD or sta == wifi.STA_APNOTFOUND or sta == wifi.STA_FAIL) then
-		led = wifid.counter % 2
-		wifid.gotip = false
+		led = self.counter % 2
+		self.gotip = false
 		
 		if (sta == wifi.STA_WRONGPWD) then
-			msg = { "wrong password" }
+			self.msg.messages = { "wrong password" }
 		elseif (sta == wifi.STA_APNOTFOUND) then
-			msg = { "wifi AP not found" }
+			self.msg.messages = { "wifi AP not found" }
 		elseif (sta == wifi.STA_FAIL) then
-			msg = { "wifi failed" }
+			self.msg.messages = { "wifi failed" }
 		end
 		
 		wifi.sta.disconnect()
 	elseif (sta == wifi.STA_GOTIP) then
 		ip, _, _ = wifi.sta.getip()
 		led = 1
-		if (wifid.gotip == false) then
-			msg = { "wifi connected" , ssid }
-			wifid.gotip = true
+		if (self.gotip == false) then
+			self.msg.messages = { "wifi connected" , ssid }
+			self.gotip = true
 		end
 	end
 	
-	wifid.flashLed(led)
-	wifid.printMessage(msg)
-	
-	wifid.counter = wifid.counter + 1
+	self:flashLed(led)
+	self.counter = self.counter + 1
 
 	if (sta == wifi.STA_WRONGPWD or sta == wifi.STA_APNOTFOUND or sta == wifi.STA_FAIL) then
-		wifid.apcounter = wifid.apcounter + 1
-		wifid.connect()
+		self.apcounter = self.apcounter + 1
+		self.connect()
 	end
 end
 
-function wifid.flashLed(led)
-	gpio.write(wifid.gpio, led)
+function Wifid:flashLed(led)
+	gpio.write(self.gpio, led)
 end
 
-function wifid.printMessage(msg)
-	--if (msg ~= nil) then
-	--	for i,v in ipairs(msg) do print(v) end
-	--end
-	
-	if (dispd ~= nil and msg ~= nil) then
-		dispd.messages = msg
-	end
-end
+return Wifid
