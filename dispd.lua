af2a77efa42bd049c44255f74ba95c25b7499231
Laszlo Dobos <dobos@complex.elte.hu>
2016-02-21 22:43:46 +0100

    Updated to work with new framework

--- dispd.lua
+++ dispd.lua
@@ -1,72 +1,76 @@
-dispd = {}
-dispd.scl = 5
-dispd.sda = 6
-dispd.disp = nil
-dispd.temperature = ""
-dispd.humidity = ""
-dispd.heat = true
-dispd.cool = false
-dispd.fan = true
-dispd.humi = true
-dispd.messages = { "initializing..." }
-dispd.tmr = 1
-dispd.interval = 1000
-dispd.counter = 0
+local Dispd = {}
+Dispd.__index = Dispd
 
-function dispd.start()
-	dispd.setTemperature(0)
-	dispd.setHumidity(0)
-	
-	i2c.setup(0, 5, 6, i2c.SLOW)
-	dispd.disp = u8g.ssd1306_128x64_i2c(0x3c)
-	dispd.disp:begin()
-	dispd.render()
-	
-	tmr.alarm(dispd.tmr, dispd.interval, 1, dispd.render)
+function Dispd.new(scl, sda, dhtd)	
+	local self = setmetatable({}, Dispd)
+	self.dhtd = dhtd
+	self.scl = scl
+	self.sda = sda
+	self.tmr = 1
+	self.interval = 1000
+	self.counter = 0
+
+	self.heat = true
+	self.cool = false
+	self.fan = true
+	self.humi = true
+	self.messages = { "initializing..." }
+	return self
 end
 
-function dispd.render()
+function Dispd:start()
+	i2c.setup(0, self.scl, self.sda, i2c.SLOW)
+	self.disp = u8g.ssd1306_128x64_i2c(0x3c)
+	self.disp:begin()
+	self:render()
+	
+	tmr.alarm(self.tmr, self.interval, 1, self:getCallback())
+end
+
+function Dispd:getCallback()
+	return function()
+		self:render()
+	end
+end
+
+function Dispd:render()
 	local big
 	
-	if (math.floor(dispd.counter / 5) % 2 == 0) then
-		big = dispd.temperature
-	else
-		big = dispd.humidity
+	if (self.dhtd ~= nil) then
+		if (math.floor(self.counter / 5) % 2 == 0) then
+			big = self.dhtd.temperature
+		else
+			big = self.dhtd.humidity
+		end
 	end
 	
-	local msg = (dispd.messages[dispd.counter % #dispd.messages + 1])
+	local msg = (self.messages[self.counter % #self.messages + 1])
 
-	dispd.disp:firstPage()
+	self.disp:firstPage()
 	repeat
 		-- temp or humi
-		dispd.disp:setFont(u8g.font_fub30)
-		dispd.disp:drawStr(0, 32, big)
-		dispd.disp:setFont(u8g.font_6x10)
+		self.disp:setFont(u8g.font_fub30)
+		self.disp:drawStr(0, 32, big)
+		self.disp:setFont(u8g.font_6x10)
 		if (msg ~= nil) then
-			dispd.disp:drawStr(0, 62, msg)
+			self.disp:drawStr(0, 62, msg)
 		end
-		dispd.disp:setFont(u8g.font_unifont_78_79)
-		if (dispd.cool) then
-			dispd.disp:drawStr(112, 16, "D")
+		self.disp:setFont(u8g.font_unifont_78_79)
+		if (self.cool) then
+			self.disp:drawStr(112, 16, "D")
 		end
-		if (dispd.heat) then
-			dispd.disp:drawStr(112, 16, "Á")
+		if (self.heat) then
+			self.disp:drawStr(112, 16, "Á")
 		end
-		if (dispd.fan) then
-			dispd.disp:drawStr(112, 38, "C")
+		if (self.fan) then
+			self.disp:drawStr(112, 38, "C")
 		end
-		if (dispd.humi) then
-			dispd.disp:drawStr(112, 60, "(")
+		if (self.humi) then
+			self.disp:drawStr(112, 60, "(")
 		end
-	until dispd.disp:nextPage() == false
+	until self.disp:nextPage() == false
 	
-	dispd.counter = dispd.counter + 1
+	self.counter = self.counter + 1
 end
 
-function dispd.setTemperature(temp)
-	dispd.temperature = string.format("%.1f°", temp)
-end
-
-function dispd.setHumidity(humi)
-	dispd.humidity = string.format("%d%%", humi)
-end
+return Dispd
