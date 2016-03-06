d6bee35865d11826086a6c10c1fb3adbca5aa952
Laszlo Dobos <dobos@complex.elte.hu>
2016-03-06 20:10:17 +0100

    reconnect logic added.

--- src/esp/mqttd.lua
+++ src/esp/mqttd.lua
@@ -1,11 +1,14 @@
 local Mqttd = {}
 Mqttd.__index = Mqttd
 
-function Mqttd.new(server, port, user, pass)
+function Mqttd.new(server)
 	local self = setmetatable({}, Mqttd)
 	self.online = false
+	self.server = server
+	self.tmr = 3
+	self.port = 1883
 	self.user = "esp"
-	self.pass = nil
+	self.pass = "esp"
 	self.client = nil
 	self.handlers = {}
 	return self
@@ -13,24 +16,45 @@ end
 
 function Mqttd:start()
 	self.client = mqtt.Client("esp-" .. node.chipid(), 120, self.user, self.pass)
-	self.client:on("offline", self:getCallback)
-end
-
-function Mqttd:getCallback()
-	return function() 
-		-- tmr to reconnect
-	end
+	
+	self.client:on("connect", function()
+		print("connected")
+		tmr.unregister(self.tmr)
+		online = true
+	end)
+	
+	self.client:on("message", function()
+		print("message recieved")
+	end)
+	
+	self.client:on("offline", function()
+		print("offline, trying to reconnect")
+		online = false
+		self:connect()
+	end)
+	
+	self:connect()
 end
 
 function Mqttd:connect()
-	self.client:connect(self.server, self.port)
+	tmr.alarm(self.tmr, 5000, 1, self:getCallback())
+end
+
+function Mqttd:getCallback()
+	return function()
+		print("connecting")
+		self.client:connect(self.server, self.port)
+	end
 end
 
 function Mqttd:publish(topic, payload)
-	self.client:publish(topic, payload, 0, 0)
+	if online then
+		self.client:publish(topic, payload, 0, 0)
+	end
 end
 
 function Mqttd:subscribe(topic, handler)
+	
 end
 
 return Mqttd
